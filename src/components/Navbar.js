4495db016462c6f89cdaba836bb367e9cff4b4e1
Mayank <palmayank2100@gmail.com>
2023-08-05 14:34:07 +0530

    Add basic navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Navbar extends Component {
   render() {
     return (
-      <h1>Navbar</h1>
-    )
+      <>
+        <nav class="navbar fixed-top"  style={{backgroundColor: "#e3f2fd", color: "black"}}>
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+              News Monkey
+            </a>
+          </div>
+        </nav>
+      </>
+    );
   }
 }
