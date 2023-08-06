da7bb926bc85187748e026c400a0571112503815
Mayank <palmayank2100@gmail.com>
2023-08-06 13:09:00 +0530

    Add null checking for news images

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,13 +5,19 @@ export default class NewsItem extends Component {
   render() {
     let { title, description, imageUrl, newsUrl } = this.props;
 
-    if (title.length > 80) {
-      title = title.slice(0, 80) + "...";
+    if (title.length > 65) {
+      title = title.slice(0, 65) + "...";
     }
 
     if (description.length > 140) {
       description = description.slice(0, 140) + "...";
     }
+
+    if (imageUrl === null) {
+      imageUrl =
+        "https://www.thermaxglobal.com/wp-content/uploads/2020/05/image-not-found.jpg";
+    }
+
     return (
       <div className="container card my-3" style={{ width: "25rem" }}>
         <div className="col mt-2 px-1">
