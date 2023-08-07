f6283d8d0d67665e0c76c58d0e851247d5ca0435
Mayank <palmayank2100@gmail.com>
2023-08-07 21:16:53 +0530

    Add page navigation

--- src/components/News.js
+++ src/components/News.js
@@ -4,23 +4,56 @@ import NewsItem from "./NewsItem";
 export default class News extends Component {
   constructor() {
     super();
-    console.log("Hello I am a constructor from News Component");
     this.state = {
-      articles: this.articles,
+      articles: [],
       loading: false,
+      page: 1,
+      totalResults: 0,
     };
   }
 
   async componentDidMount() {
     let url =
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=2aebf6ff38d648849b5ad39a03a55042";
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=2aebf6ff38d648849b5ad39a03a55042&page=1&pagesize=18";
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({ articles: parsedData.articles });
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
   }
-  
+
+  async componentDidUpdate() {}
+
+  handlePreviousClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2aebf6ff38d648849b5ad39a03a55042&page=${
+      this.state.page - 1
+    }&pagesize=18`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({ page: this.state.page - 1, articles: parsedData.articles });
+
+    window.scrollTo(0, 0);
+  };
+
+  handleNextClick = async () => {
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 18)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2aebf6ff38d648849b5ad39a03a55042&page=${
+        this.state.page + 1
+      }&pagesize=18`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
+
+      window.scrollTo(0, 0);
+    }
+  };
+
   render() {
-    
     return (
       <div className="container">
         <div className="container row col-md-auto mx-auto">
@@ -35,6 +68,26 @@ export default class News extends Component {
               />
             );
           })}
+          <div className="d-flex justify-content-between my-3">
+            <button
+              className="btn btn-primary"
+              style={{ width: "8rem" }}
+              disabled={this.state.page <= 1}
+              onClick={this.handlePreviousClick}
+            >
+              &larr; Previous
+            </button>
+            <button
+              className="btn btn-primary"
+              style={{ width: "8rem" }}
+              disabled={
+                this.state.page + 1 > Math.ceil(this.state.totalResults / 18)
+              }
+              onClick={this.handleNextClick}
+            >
+              Next &rarr;
+            </button>
+          </div>
         </div>
       </div>
     );
